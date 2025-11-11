02df7e62ea8eab46e028d19110f1d553d3d1dcb1
Emeter Victor <93406217+pythonvista@users.noreply.github.com>
2025-11-11 01:02:38 -0800

    cbbb

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = "127.0.0.1"; // Changed from localhost
+$host = "db.pxxl.pro"; // Changed from localhost
 $user = "user_661773b4";
 $pass = "1c39d8a5fadf2d6730d46844fa22346f";
 $dbname = "db_0ed9c61a";
-$port = 3306; // Explicitly specify port
+$port = 29271; // Explicitly specify port
 
 $db = new mysqli($host, $user, $pass, $dbname, $port);
 
